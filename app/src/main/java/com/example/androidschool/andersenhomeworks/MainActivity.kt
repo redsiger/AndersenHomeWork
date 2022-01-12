7d14ddc7775c61c5d817527d8d5b827d64fe475f
redsiger <redsiger@yandex.ru>
2022-01-12 23:06:26 +0300

    Completed Part A of lesson 1.2 of Google Fundamentals Codelab

--- app/src/main/java/com/example/androidschool/andersenhomeworks/MainActivity.kt
+++ app/src/main/java/com/example/androidschool/andersenhomeworks/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.androidschool.andersenhomeworks
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 val MAIN_ACTIVITY_TAG = MainActivity::class.java.name
 
@@ -17,5 +20,38 @@ class MainActivity : AppCompatActivity() {
         Log.i(MAIN_ACTIVITY_TAG, "Hello world! from info log")
         Log.w(MAIN_ACTIVITY_TAG, "Hello world! from warn log")
         Log.e(MAIN_ACTIVITY_TAG, "Hello world! from error log")
+
+        fundamentals_1_2()
+    }
+
+    /**
+     * Codelab Fundamentals 1.2
+     */
+
+    private var mCount = 0
+
+    private fun fundamentals_1_2() {
+
+        val btnToast = findViewById<Button>(R.id.activityMain_btnToast)
+        btnToast.setOnClickListener {
+            showToast()
+        }
+
+        val tvCount = findViewById<TextView>(R.id.activityMain_tvCount)
+
+        val btnCount = findViewById<Button>(R.id.activityMain_btnCount)
+        btnCount.setOnClickListener {
+            countUp(tvCount)
+        }
+
+    }
+
+    private fun showToast() {
+        Toast.makeText(this, R.string.activityMain_toastMessage, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun countUp(view: TextView) {
+        mCount++
+        view.text = mCount.toString()
     }
 }
